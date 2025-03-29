978d38774d90054bdfc8f44d42c0702d9ac175a9
EC2 Default User <ec2-user@ip-172-31-40-118.ap-northeast-1.compute.internal>
2025-03-29 04:18:56 +0000

    実装フェーズ完了

--- app/controllers/public/golf_courses_controller.rb
+++ app/controllers/public/golf_courses_controller.rb
@@ -1,5 +1,9 @@
 class Public::GolfCoursesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   def index
+    @golf_courses = GolfCourse.all
   end
 
   def show
@@ -23,18 +27,37 @@ class Public::GolfCoursesController < ApplicationController
   end
 
   def edit
+    @golf_course = GolfCourse.find(params[:id])
   end
 
   def update
+    @golf_course = GolfCourse.find(params[:id])
+
+    if @golf_course.update(golf_course_params)
+      flash[:notice] = "編集しました"
+      redirect_to @golf_course
+    else
+      flash.now[:alert] = "編集に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
+    @golf_course = GolfCourse.find(params[:id])
+    @golf_course.destroy
+    flash[:notice] = "削除しました"
+    redirect_to golf_courses_path
   end
 
   private
 
   def golf_course_params
-    params.require(:golf_course).permit(:name, :caption, :body, :addres, :latitude, :longitude, :star)
+    params.require(:golf_course).permit(:name, :caption, :body, :address, :latitude, :longitude, :star, :image)
+  end
+
+  def correct_user
+    @golf_course = current_user.golf_courses.find_by_id(params[:id])
+    redirect_to root_path unless @golf_course
   end
 end
 
